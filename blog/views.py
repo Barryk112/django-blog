c8c3c5d3a2fce9816e38efd16ac84cfbedcc8454
Barryk112 <barrykiely112@gmail.com>
2023-05-04 22:29:48 +0000

    Updated index.html to show blog post and added logic to paginated list

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,6 @@ from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by('-create_on')
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6

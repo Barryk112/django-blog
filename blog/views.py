10fe4e4a1fe30350cdc41ce45a5dd3c9b6894d26
Barryk112 <barrykiely112@gmail.com>
2023-05-06 12:53:07 +0000

    Added the function to like and unlike posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 
@@ -69,3 +70,22 @@ class PostDetail(View):
                 "comment_form": comment_form,
             },
         )
+
+
+class PostLike(View):
+
+    def post(slef, request, slug):
+        # Gets post in question
+        post = get_object_or_404(Post, slug=slug)
+
+        # Checks if post is already liked
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+
+        else:
+            post.likes.add(request.user)
+
+        # Reloads the page
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+        

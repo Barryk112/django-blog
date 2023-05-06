3b59df2c92fb1273a9589ec3058e25f9debaa415
Barryk112 <barrykiely112@gmail.com>
2023-05-06 10:13:01 +0000

    Installed crispy forms, added a from for comments, updated post_detail to show comments box for users

--- codestar/settings.py
+++ codestar/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
     'blog',
 ]
 
@@ -60,6 +61,8 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
